16a90311dfcc685cddacf3e9090898e1921543d1
usstudio <69433152+usstudio7744@users.noreply.github.com>
2022-11-23 11:28:41 +0500

    initial commit

--- handlers/users/telegraph.py
+++ handlers/users/telegraph.py
@@ -7,7 +7,7 @@ from loader import bot
 async def photo_handler(msg:types.Message):
     photo=msg.photo[-1]
     link = await photo_link(photo)
-    await msg.answer("Tez orada natija jo'natiladi iltimos kutib turing...")
+    await msg.answer("Tez orada fayl jo'natiladi\nIltimos kutib turing...")
     result=await text_recognition(link)
 
     await msg.answer(result)
@@ -15,3 +15,6 @@ async def photo_handler(msg:types.Message):
     await bot.send_document(msg.chat.id,document=res_doc)
     # await msg.reply_document(document='result.txt',caption="doc")
 
+@dp.message_handler(types.Message)
+async def message_handler(msg:types.Message):
+    await msg.reply("Iltimos rasm jo'nating! \n Plase send photo!")
